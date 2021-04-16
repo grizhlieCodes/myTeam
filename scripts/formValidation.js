b7accc2e42bb16742e7591212416a3724150b645
grizhlieCodes <rafal.potasz@gmail.com>
2021-04-16 14:59:27 +0100

    Fixed form validation bug

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -9,8 +9,9 @@ let errorRequiredMessage = "This field is required"
 let errorLengthMessage = "This field is too short"
 
 formItems.forEach(item => {
-    item.addEventListener('keyup', () => {
-        checkValidity(item)
+    item.addEventListener('keyup', (e) => {
+        console.log(e)
+        checkValidity(item) 
     })
 })
 
@@ -35,7 +36,10 @@ function updateName(input){
 }
 
 
-function checkValidity(item, e){
+
+let emailValid = false;
+let nameValid = false;
+function checkValidity(item) {
     let input = item.children[0]
     let label = item.children[1]
     let span = item.children[2]
@@ -43,53 +47,63 @@ function checkValidity(item, e){
 
     updateName(input)
 
-    if (input.checkValidity() && input.value.length > 5) {
+    if (input.checkValidity() && input.name == "email" && input.value.length >= 3) {
         item.classList.remove('any-error')
         errMes.classList.remove('show-error')
         span.classList.remove('error')
+        emailValid = true
+    } 
+
+    if(input.checkValidity() && input.name == "name" && input.value.length >= 3){
+        item.classList.remove('any-error')
+        errMes.classList.remove('show-error')
+        span.classList.remove('error')
+        nameValid = true
+    }
+
+    if(emailValid && nameValid){
         errorFreeShowModal = true
     } else {
-        if(input.checkValidity() && input.value.length <= 0 && e.type == "submit"){
-            item.classList.add('any-error')
-            errMes.classList.add('show-error')
-            span.classList.add('error')
-            errMes.textContent = errorRequiredMessage
-            errorFreeShowModal = false
-            return
-        }
-        if(!input.checkValidity() || input.value.length < 5){
-            item.classList.add('any-error')
-            errMes.classList.add('show-error')
-            span.classList.add('error')
-            errorFreeShowModal = false
-        }
-        if (input.value.length < 5) {
-            errMes.textContent = errorLengthMessage
-            span.classList.add('error')
-            errorFreeShowModal = false
-        } 
-        if (!input.checkValidity() && input.name == "email" && input.value.length >= 5){
-            console.log('email invalid')
-            errMes.textContent = errorEmailMessage
-            span.classList.add('error')
-            errorFreeShowModal = false
-        }
+        errorFreeShowModal = false
+    }
+
+    if(input.checkValidity() && input.value.length == 0){
+        item.classList.add('any-error')
+        errMes.classList.add('show-error')
+        span.classList.add('error')
+        errMes.textContent = errorRequiredMessage
+        return
+    }
+
+    if(!input.checkValidity() || input.value.length < 3){
+        item.classList.add('any-error')
+        errMes.classList.add('show-error')
+        span.classList.add('error')
+    }
+    if (input.value.length < 3) {
+        errMes.textContent = errorLengthMessage
+    } 
+    if (input.value.length < 3 && input.name == "name") {
+        nameValid = false
+    } 
+    if (!input.checkValidity() && input.name == "email" && input.value.length >= 5){
+        errMes.textContent = errorEmailMessage
+        emailValid = false
     }
 }
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(e)
     formItems.forEach(item => {
         checkValidity(item, e)
     })
     if(errorFreeShowModal){
         modalMessage.textContent = `Message received ${modalFirstName}! 
         We'll get back to you asap.`
-        modal.classList.add('display-true')
+        modal.classList.add('display-true') //add display: grid
         setTimeout(() => {
             modal.classList.add('active')
-        },150)
+        },50)
     }
 })
